9b7f1112ce33c5bb9d6d1c99d59f92680358b077
Sumanth Paila <134701181+nomadicfoe@users.noreply.github.com>
2026-04-21 15:16:34 -0700

    Update YouTube video source in about page

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -23,7 +23,7 @@ export default function AboutPage() {
             <iframe
               width="100%"
               height="100%"
-              src="https://www.youtube.com/embed/YOUR_VIDEO_ID_HERE"
+              src="https://youtu.be/PpmYUbW4LSI"
               title="LocoFinder Demo"
               frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
